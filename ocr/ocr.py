4a91c6dde0d1a8f78c28562c5b6eff9014b4823f
ProtonCoder <51157245+ProtonCoder23@users.noreply.github.com>
2020-09-12 21:45:42 +0530

    Add files via upload

---
+++ ocr/ocr.py
@@ -0,0 +1,40 @@
+import pytesseract
+import cv2
+import pyscreenshot as ImageGrab
+import numpy as np
+import time
+cap = cv2.VideoCapture(-1)
+cap.set(3, 640)
+cap.set(4, 480)
+
+
+def CaptureScreen(bbox=(300, 300, 1500, 1000)):
+    capscr = np.array(ImageGrab.grab(bbox))
+    capscr = cv2.cvtColor(capscr , cv2.COLOR_RGB2BGR)
+    return capscr
+
+
+while True:
+    timer = cv2.getTickCount()
+    _, img = cap.read()
+    himg, wimg, _ = img.shape
+    boxes = pytesseract.image_to_boxes(img)
+
+    for b in boxes.splitlines():
+        b = b.split(' ')
+        #print(b)
+        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+        cv2.rectangle(img, (x, himg - y), (w, himg - h), (50, 50, 255), 1)
+
+    cv2.imshow('Result', img)
+    cv2.waitKey(1)
+
+cv2.imshow('Image', img)
+cv2.waitkey(0)
+# img = cv2.imread('sample.jpg')
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#print(pytesseract.image_to_string(img))
+
+
+
+
